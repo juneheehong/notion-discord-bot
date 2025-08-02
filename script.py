4c75e78e504dfd15bdfd04833bf71e7853159f77
junehee.hong <junehee.hong@databean.co.kr>
2025-08-02 14:22:23 +0900

    init

--- script.py
+++ script.py
@@ -2,10 +2,13 @@ import os
 import requests
 from datetime import datetime, timezone, timedelta
 # 로컬에서 실행시 dotenv import
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 # 로컬에서 실행시 .env 파일 로드
-# load_dotenv()
+load_dotenv()
+
+test = os.getenv('DISCORD_WEBHOOK_URL')
+print(test)
 
 def fetch_notion_data():
     notion_api_key = os.getenv('NOTION_API_KEY')
